71d3aa024e036d56adc3f10f40d252b4a164e2e3
maruware <me@maruware.com>
2023-03-10 02:45:22 +0900

    Add token

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maruware/go-interpreter
+
+go 1.19
---
+++ token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCITON = "FUNCTION"
+	LET      = "LET"
+)
